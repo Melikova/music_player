18d84b73393baeea3d6d7fd48c74086d6e9e3ed9
Mehin Melikova <mhmelikova@gmail.com>
2023-05-22 17:48:59 +0400

    mp3 list array added

---
+++ main.js
@@ -0,0 +1,23 @@
+const mp3 = [
+    {
+        id: 001,
+        artist: "Emmanuel Moir",
+        mp3: "2.mp3",
+        name: 'Beau Malheur',
+        youtube: 'https://www.youtube.com/watch?v=EHkn5bjm3nI',
+        photo: "https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"
+    },
+    {
+        id: 002,
+        artist: "Emmanuel Moir",
+        mp3: "3.mp3",
+        name: 'Beau Malheur',
+        youtube: 'https://www.youtube.com/watch?v=EHkn5bjm3nI',
+        photo: "https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg"
+    }
+];
+
+const favourites = [001];
+
+
+
